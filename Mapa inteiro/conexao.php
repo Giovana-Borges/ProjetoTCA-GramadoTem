7dde199cb34b9cde8d8ecfc8bfd4c43849b588e3
Giovana-Borges <98786593+Giovana-Borges@users.noreply.github.com>
2022-02-09 21:12:25 -0300

    Delete Mapa inteiro directory

--- Mapa inteiro/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "G14B03c2002.";
-	$dbname = "location";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
